60cde5d8ef344a72d5ae450c852687853c122495
masari <johnlloyd42344@gmail.com>
2024-09-17 15:58:28 +0800

    first commit

---
+++ Arrays.java
@@ -0,0 +1,30 @@
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        
+       char[][] prac = {
+           {'*','*','*','*','*'},
+           {'*','*','*','*','*'},  
+           {'*','*','*','*','*'},
+           {'*','*','*','*','*'},
+           {'*','*','*','*','*'},
+                
+           };      
+                 
+        for (int i=0; i <prac.length; i++){
+         System.out.println(); 
+            for (int j=0;j< prac[i].length; j++) {
+                System.out.print(prac[i][j]);   
+ 
+            }
+        }
+    }
+}
+       
+        
+          
+        
+
+    
+
