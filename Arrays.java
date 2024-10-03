75272066d71b2f8b68e760e4fa68c3c7fd03f474
Andrion <20201428@nbsc.edu.ph>
2024-10-03 21:05:36 +0800

    LinkedList

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        
+       int[] simpleArray = {1, 2, 3, 4, 5};
+        System.out.println("Simple Array: ");
+        for (int i : simpleArray) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
